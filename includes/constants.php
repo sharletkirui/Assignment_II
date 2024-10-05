99c093ae2900e674a0174162f0988f6c8d84d9c2
sharletkirui <sharlet.kirui@strathmore.edu>
2024-10-05 20:26:42 +0300

    Email verification

--- includes/constants.php
+++ includes/constants.php
@@ -11,3 +11,11 @@ define('DBNAME', 'assignment_2');
 $protocol = isset($_SERVER['HTTPS']) && 
 $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
 $base_url = $protocol . $_SERVER['HTTP_HOST'] . '/';
+$conf['ver_code_time'] = date("Y-m-d H:i:s", strtotime("+ 1 days"));
+$conf['verification_code'] = rand(10000,99999);
+$conf['site_initials'] = "Admin App";
+$conf['site_url'] = "$base_url/". DBNAME;
+
+
+// verify that the email domain is authorized (@strathmore.edu, @gmail.com, @yahoo.com, @mada.co.ke) and not (@yanky.net)
+$conf['valid_domains'] = ["strathmore.edu", "gmail.com", "yahoo.com", "mada.co.ke", "outlook.com"];
