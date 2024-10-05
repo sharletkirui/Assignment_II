150022a956c5f7828a7475641081511c31c0bfb8
sharletkirui <sharlet.kirui@strathmore.edu>
2024-10-05 20:26:15 +0300

    Base url

--- includes/constants.php
+++ includes/constants.php
@@ -8,4 +8,6 @@ define('DBPORT', '3306');
 define('HOSTUSER', 'root');
 define('HOSTPASS', '');
 define('DBNAME', 'assignment_2');
-
+$protocol = isset($_SERVER['HTTPS']) && 
+$_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
+$base_url = $protocol . $_SERVER['HTTP_HOST'] . '/';
