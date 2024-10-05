a37aadb2ffd2ee21704ecb8996e383e54584353e
sharletkirui <sharlet.kirui@strathmore.edu>
2024-10-05 20:24:43 +0300

    Constants for Database Connection

---
+++ includes/constants.php
@@ -0,0 +1,11 @@
+<?php
+
+date_default_timezone_set("AFRICA/Nairobi");
+
+define('DBTYPE', 'PDO');
+define('HOSTNAME', 'localhost');
+define('DBPORT', '3306');
+define('HOSTUSER', 'root');
+define('HOSTPASS', '');
+define('DBNAME', 'assignment_2');
+
